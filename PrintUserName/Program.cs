e8468da9e4d14fe758bb14b84186bbd46f620ce2
Matthew <Mattrj99@gmail.com>
2023-04-28 14:31:52 -0400

    Provide beginner example of getting user input and displaying to console

--- PrintUserName/Program.cs
+++ PrintUserName/Program.cs
@@ -10,6 +10,27 @@ namespace PrintUserName
     {
         static void Main(string[] args)
         {
+            //Sample program to demostrate how to display user name
+            //Part of tutorial series
+
+            //Possible upgrades to this program is to handle user input. User can currently enter numbers
+            //Could check if input is numbers, length of the input
+
+            string userName; //declaring the string variable userName so we can assign it later
+
+            //Writing to the console to tell the user what to enter
+            Console.WriteLine("Hello, what is your name?: ");
+
+            //Whatever the user types will be assigned to the variable userName
+            userName = Console.ReadLine();
+
+            //We now have the input from the user so we need to display it back
+
+            Console.WriteLine("Nice to meet you, " +  userName);
+            //The output displays the string we hard coded 'Nice to meet you, ' AND our variable right after
+
+            //This is so the console window doesnt close
+            Console.ReadLine();
         }
     }
 }
